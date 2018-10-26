967efd5eae946a1786e41905f8dc22261786d0bf
aaronchenwei <aaronchenwei@users.noreply.github.com>
2018-10-26 13:20:42 +0800

    Update tslint.js

--- tslint.js
+++ tslint.js
@@ -1,11 +1,13 @@
+const path = require('path');
+
 module.exports = {
   defaultSeverity: 'warning',
-  rulesDirectory: ['tslint-plugin-prettier'],
-  extends: [
-    'tslint:latest',
-    'tslint-react',
-    'tslint-config-airbnb',
-    'tslint-config-prettier',
+  rulesDirectory: [
+    'tslint-plugin-prettier',
+    'tslint-consistent-codestyle',
+    'tslint-eslint-rules',
+    path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './'),
+    path.join(path.dirname(require.resolve('tslint-react')), 'rules'),
   ],
   rules: {
     /**
@@ -19,12 +21,50 @@ module.exports = {
       },
     ],
 
-    // override default rules
-    'array-type': [true, 'array'],
-
+    /**
+     * tslint:recommanded
+     */
+    'adjacent-overload-signatures': true,
+    align: false,
+    'array-type': {
+      options: ['array'],
+    },
+    'arrow-parens': false,
     'arrow-return-shorthand': true,
-
-    'import-name': false, // #tslint-microsoft-contrib
+    'ban-types': {
+      options: [
+        ['Object', 'Avoid using the `Object` type. Did you mean `object`?'],
+        [
+          'Function',
+          'Avoid using the `Function` type. Prefer a specific function type, like `() => void`.',
+        ],
+        ['Boolean', 'Avoid using the `Boolean` type. Did you mean `boolean`?'],
+        ['Number', 'Avoid using the `Number` type. Did you mean `number`?'],
+        ['String', 'Avoid using the `String` type. Did you mean `string`?'],
+        ['Symbol', 'Avoid using the `Symbol` type. Did you mean `symbol`?'],
+      ],
+    },
+    'callable-types': true,
+    'class-name': true,
+    'comment-format': {
+      options: ['check-space'],
+    },
+    curly: true,
+    'cyclomatic-complexity': false,
+    eofline: false,
+    forin: true,
+    'import-spacing': false,
+    indent: false,
+    'interface-name': {
+      options: ['always-prefix'],
+    },
+    'interface-over-type-literal': true,
+    'jsdoc-format': true,
+    'label-position': true,
+    'max-classes-per-file': {
+      options: [1],
+    },
+    'max-line-length': false,
     'member-access': [true, 'no-public'],
     'member-ordering': [
       true,
@@ -38,57 +78,107 @@ module.exports = {
         ],
       },
     ],
-    'no-boolean-literal-compare': false,
+    'new-parens': false,
+    'no-angle-bracket-type-assertion': true,
+    'no-any': false,
+    'no-arg': true,
+    'no-bitwise': true,
+    'no-conditional-assignment': true,
+    'no-consecutive-blank-lines': false,
     'no-console': false,
-    'no-implicit-dependencies': false,
+    'no-construct': true,
+    'no-debugger': true,
+    'no-duplicate-super': true,
+    'no-empty': true,
+    'no-empty-interface': true,
+    'no-eval': true,
+    'no-internal-module': true,
+    'no-invalid-this': false,
+    'no-misused-new': true,
+    'no-namespace': true,
     'no-null-keyword': true,
-    'no-submodule-imports': false,
+    'no-parameter-properties': false,
+    'no-reference': true,
+    'no-reference-import': true,
+    'no-shadowed-variable': true,
+    'no-string-literal': true,
+    'no-string-throw': true,
+    'no-switch-case-fall-through': false,
+    'no-trailing-whitespace': false,
+    'no-unnecessary-initializer': true,
+    'no-unsafe-finally': true,
+    'no-unused-expression': true,
+    // disable this rule as it is very heavy performance-wise and not that useful
+    'no-use-before-declare': false,
+    'no-var-keyword': true,
+    'no-var-requires': true,
+    'object-literal-key-quotes': false,
+    'object-literal-shorthand': true,
     'object-literal-sort-keys': false,
+    'one-line': false,
+    'one-variable-per-declaration': {
+      options: ['ignore-for-loop'],
+    },
+    'only-arrow-functions': {
+      options: ['allow-declarations', 'allow-named-functions'],
+    },
+    'ordered-imports': {
+      options: {
+        'import-sources-order': 'case-insensitive',
+        'module-source-path': 'full',
+        'named-imports-order': 'case-insensitive',
+      },
+    },
+    'prefer-const': true,
+    'prefer-for-of': true,
     'prefer-template': [true, 'allow-single-concat'],
-    'triple-equals': [true, 'allow-undefined-check'],
-    'variable-name': [
-      true,
-      'ban-keywords',
-      'check-format',
-      'allow-pascal-case',
-    ],
+    quotemark: false,
+    radix: true,
+    semicolon: false,
+    'space-before-function-paren': false,
+    'trailing-comma': false,
+    'triple-equals': {
+      options: ['allow-undefined-check'],
+    },
+    typedef: false,
+    'typedef-whitespace': false,
+    'typeof-compare': false,
+    'unified-signatures': true,
+    'use-isnan': true,
+    'variable-name': {
+      options: ['ban-keywords', 'check-format', 'allow-pascal-case'],
+    },
+    whitespace: false,
 
     /**
      * tslint-react
      */
-    // default tslint rules
-    /*
-    'jsx-alignment': true,
-    'jsx-boolean-value': true,
-    'jsx-curly-spacing': [true, 'never'],
-    'jsx-equals-spacing': [true, 'never'],
+    'jsx-alignment': false,
+    'jsx-ban-elements': false,
+    'jsx-ban-props': false,
+    'jsx-boolean-value': [true, 'never'],
+    'jsx-curly-spacing': false,
+    'jsx-equals-spacing': false,
     'jsx-key': true,
     'jsx-no-bind': true,
     'jsx-no-lambda': true,
-    'jsx-no-multiline-js': true,
+    'jsx-no-multiline-js': false,
     'jsx-no-string-ref': true,
     'jsx-self-close': true,
-    'jsx-wrap-multiline': true,
-    */
-
-    // not configured in default
-    /*
-    'jsx-ban-elements': false,
-    'jsx-ban-props': false,
-    'jsx-use-translation-function': false,
     'jsx-space-before-trailing-slash': false,
-    */
-
-    // override tslint rules
-    'jsx-boolean-value': [true, 'never'],
+    'jsx-wrap-multiline': false,
+    'jsx-use-translation-function': false,
 
     /**
-     * tslint-config-prettier
+     * tslint-microsoft-contrib
      */
-    'jsx-alignment': false,
-    'jsx-curly-spacing': false,
-    'jsx-equals-spacing': false,
-    'jsx-no-multiline-js': false,
-    'jsx-wrap-multiline': false,
+    'no-function-constructor-with-string-args': true,
+    'import-name': false,
+    'prefer-array-literal': true,
+
+    /**
+     * tslint-consistent-codestyle
+     */
+    'object-shorthand-properties-first': true,
   },
 };
