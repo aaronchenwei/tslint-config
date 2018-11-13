1b3144cc2a17b16bc6ef1d3fb98ae6de6a8ad7d6
aaronchenwei <aaronchenwei@hotmail.com>
2018-11-13 20:24:18 +0800

    issue #67

--- tslint.js
+++ tslint.js
@@ -178,76 +178,89 @@ module.exports = {
     /**
      * tslint-microsoft-contrib
      */
+
+    /**
+     * Security Rules. The following rules should be turned on because they find security issues
+     * or are recommended in the Microsoft Secure Development Lifecycle (SDL)
+     */
+    'insecure-random': true,
+    'no-banned-terms': true,
+    'no-cookies': true,
+    'no-delete-expression': true,
+    'no-disable-auto-sanitization': true,
+    'no-document-domain': true,
+    'no-document-write': true,
+    'no-exec-script': true,
+    'no-function-constructor-with-string-args': true,
+    'no-http-string': [
+      true,
+      'http://www.example.com/?.*',
+      'http://localhost:?.*',
+    ],
+    'no-inner-html': true,
+    'no-octal-literal': true,
+    'no-reserved-keywords': true,
+    'no-string-based-set-immediate': true,
+    'no-string-based-set-interval': true,
+    'no-string-based-set-timeout': true,
+    'non-literal-fs-path': true,
+    'non-literal-require': true,
+    'possible-timing-attack': true,
+    'react-anchor-blank-noopener': true,
+    'react-iframe-missing-sandbox': true,
+    'react-no-dangerous-html': true,
+
+    /**
+     * Common Bugs and Correctness. The following rules should be turned on because they find
+     * common bug patterns in the code or enforce type safety.
+     */
+    'jquery-deferred-must-complete': true,
+    'mocha-avoid-only': false,
+    'mocha-no-side-effect-code': false,
+    'no-backbone-get-set-outside-model': false,
+    'no-constant-condition': true,
+    'no-control-regex': false,
+    'no-increment-decrement': true,
+    'no-invalid-regexp': false,
+    'no-jquery-raw-elements': true,
+    'no-regex-spaces': false,
+    'no-unnecessary-bind': true,
+    'no-unnecessary-override': true,
+    'no-with-statement': true,
+    'promise-must-complete': true,
+    'react-this-binding-issue': true,
+    'react-unused-props-and-state': true,
+    'use-named-parameter': true,
+
+    /**
+     * Code Clarity. The following rules should be turned on because they make the code
+     * generally more clear to the reader.
+     */
     'chai-prefer-contains-to-index-of': false, // Chai is an assertion library. Not in use.
     'chai-vague-errors': false,
     'export-name': false,
     'function-name': false,
     'import-name': false,
     'informative-docs': true,
-    'insecure-random': true,
-    'jquery-deferred-must-complete': true,
     'max-func-body-length': false,
-    'missing-jsdoc': false, // Deprecated
-    'missing-optional-annotation': false, // Deprecated
-    'mocha-avoid-only': false,
-    'mocha-no-side-effect-code': false,
     'mocha-unneeded-done': false,
-    'no-backbone-get-set-outside-model': false,
-    'no-banned-terms': true,
-    'no-constant-condition': true,
-    'no-control-regex': false,
-    'no-cookies': true,
-    'no-delete-expression': true,
-    'no-disable-auto-sanitization': true,
-    'no-document-domain': true,
-    'no-document-write': true,
-    'no-duplicate-case': false, // Deprecated
-    'no-duplicate-parameter-names': false, // Deprecated
-    'no-empty-interfaces': false, // Deprecated
-    'no-empty-line-after-opening-brace': false,
-    'no-exec-script': true,
     'no-for-in': true,
-    'no-function-constructor-with-string-args': true,
     'no-function-expression': true,
-    'no-http-string': [
-      true,
-      'http://www.example.com/?.*',
-      'http://localhost:?.*',
-    ],
-    'no-increment-decrement': true,
-    'no-inner-html': true,
-    'no-invalid-regexp': false,
-    'no-jquery-raw-elements': true,
-    'no-missing-visibility-modifiers': false, // Deprecated
     'no-multiline-string': false,
-    'no-multiple-var-decl': false, // Deprecated
-    'no-octal-literal': true,
-    'no-regex-spaces': false,
     'no-relative-imports': false, // TODO
-    'no-reserved-keywords': true,
-    'no-single-line-block-comment': true,
-    'no-stateless-class': false, // Deprecated
-    'no-string-based-set-immediate': true,
-    'no-string-based-set-interval': true,
-    'no-string-based-set-timeout': true,
     'no-suspicious-comment': true, // TODO
     'no-typeof-undefined': true,
-    'no-unexternalized-strings': false, // TODO
-    'no-unnecessary-bind': true,
     'no-unnecessary-field-initialization': true,
     'no-unnecessary-local-variable': true,
-    'no-unnecessary-override': true,
-    'no-unnecessary-semicolons': false,
     'no-unsupported-browser-code': true,
     'no-useless-files': true,
-    'no-var-self': false, // Deprecated
-    'no-with-statement': true,
-    'non-literal-fs-path': true,
-    'non-literal-require': true,
-    'possible-timing-attack': true,
     'prefer-array-literal': true,
-    'prefer-type-cast': false, // TODO
-    'promise-must-complete': true,
+    'underscore-consistent-invocation': true,
+
+    /**
+     * Accessibility. The following rules should be turned on to guarantee the best user
+     * experience for keyboard and screen reader users.
+     */
     'react-a11y-anchors': true,
     'react-a11y-aria-unsupported-elements': true,
     'react-a11y-event-has-role': true,
@@ -265,15 +278,34 @@ module.exports = {
     'react-a11y-role-supports-aria-props': true,
     'react-a11y-tabindex-no-positive': true,
     'react-a11y-titles': true,
-    'react-anchor-blank-noopener': true,
-    'react-iframe-missing-sandbox': true,
-    'react-no-dangerous-html': true,
-    'react-this-binding-issue': true,
-    'react-tsx-curly-spacing': false, // Deprecated
-    'react-unused-props-and-state': true,
-    'underscore-consistent-invocation': true,
-    'use-named-parameter': true,
-    'valid-typeof': false,
+
+    /**
+     * Whitespace related rules. The only recommended whitespace strategy is to pick a single format and
+     * be consistent.
+     */
+    'no-empty-line-after-opening-brace': false,
+    'no-single-line-block-comment': true,
+    'no-unnecessary-semicolons': false,
+
+    /**
+     * Controversial/Configurable rules.
+     */
+    'no-unexternalized-strings': false, // TODO
+    'prefer-type-cast': false, // TODO
+
+    /**
+     * Deprecated rules.  The following rules are deprecated for various reasons.
+     */
+    'missing-jsdoc': false,
+    'missing-optional-annotation': false, // now supported by TypeScript compiler
+    'no-duplicate-case': false,
+    'no-duplicate-parameter-names': false, // now supported by TypeScript compiler
+    'no-empty-interfaces': false, // use tslint no-empty-interface rule instead
+    'no-missing-visibility-modifiers': false, // use tslint member-access rule instead
+    'no-multiple-var-decl': false, // use tslint one-variable-per-declaration rule instead
+    'no-stateless-class': false,
+    'no-var-self': false,
+    'react-tsx-curly-spacing': false,
 
     /**
      * tslint-consistent-codestyle
@@ -307,7 +339,7 @@ module.exports = {
     'no-empty-character-class': false,
     'no-ex-assign': true,
     'no-extra-boolean-cast': true,
-    'no-extra-semi': false, // TODO
+    'no-extra-semi': false, // conflict to  tslint-config-prettier
     'no-inner-declarations': false,
     'no-invalid-regexp': false,
     'no-regex-spaces': false,
@@ -347,7 +379,7 @@ module.exports = {
       true,
       'as-needed',
       {
-        requireReturnForObjectLiteral: true,
+        requireReturnForObjectLiteral: false,
       },
     ],
     'ter-arrow-parens': false,
@@ -355,8 +387,8 @@ module.exports = {
     'ter-prefer-arrow-callback': [
       true,
       {
-        allowNamedFunctions: true,
-        allowUnboundThis: false,
+        allowNamedFunctions: false,
+        allowUnboundThis: true,
       },
     ],
   },
